955483028a9da61a514f5a2cf3e52ffdb259e082
Recode <recodeexistance@gmail.com>
2021-03-11 00:06:16 +0000

    Paragraph now overwritten by new fact instead of appended. Added some todo stuff.

--- uselessFact_parser.js
+++ uselessFact_parser.js
@@ -5,7 +5,7 @@ function fetchData(url) {
     const parser = new DOMParser();
     const htmlString = parser.parseFromString(data, "text/html"); 
     const randomFactElement = htmlString.querySelector(".uselessfact");
-    document.write(`<p>${randomFactElement.textContent}</p>`);    
+    document.getElementById('fact-container').textContent = randomFactElement.textContent;     
     }); 
 
     xhr.open('GET', url, true); 
@@ -14,4 +14,6 @@ function fetchData(url) {
 const randomFactUrl = `https://uselessfacts.jsph.pl/random?language=en`; 
 let button = `<input type="button" value="Get Fact" href="javascript:void(0)" onclick="fetchData(randomFactUrl)"">`; 
 
-document.write(button); 
+document.write(button);
+document.write(`<div id="fact-container"><p></p></p></div>`); 
+//TODO: create an HTML or React page for the interface.  Sort out some styling
